2c29efe3a025a7d8bfa20c17ad71fb47ebcdaffb
Asel <88249718+alimbaeva@users.noreply.github.com>
2022-10-17 10:26:44 +0300

    Add files via upload

--- lesson-7/pdo.php
+++ lesson-7/pdo.php
@@ -1,3 +1,8 @@
 <?php
-$drivers = PDO::getAvailableDrivers();
-var_dump($drivers);
+$pdo = new PDO('sqlite:'.__DIR__.'/database.db');
+$query = 'CREATE TABLE `students` (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(100) NOT NULL
+         )';
+
+$statement = $pdo->query($query);  //PDOStatement
